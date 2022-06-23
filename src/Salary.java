be4812d602336c6f37da11bfd1596bb0b5b68274
Josh Mann <65541818+jlmdev@users.noreply.github.com>
2022-06-23 13:44:06 -0400

    Changes * Adding variables to generalize

--- src/Salary.java
+++ src/Salary.java
@@ -1,15 +1,26 @@
-
+/*
+Salary
+Josh Mann
+6-23-22
+ */
 
 public class Salary {
     public static void main(String [] args) {
         int hourlyWage;
+        int workHoursPerWeek;
+        int annualSalary;
+        int workWeeksPerYear;
+        int monthlySalary;
 
         hourlyWage = 20;
+        workHoursPerWeek = 35;
+        annualSalary = hourlyWage * workHoursPerWeek * 50;
+        monthlySalary = annualSalary / 12;
 
         System.out.print("Annual salary is: ");
-        System.out.println(hourlyWage * 40 * 50);
+        System.out.println(annualSalary);
 
         System.out.print("Monthly salary is: ");
-        System.out.println((hourlyWage * 40 * 50) / 12);
+        System.out.println(monthlySalary);
     }
 }
